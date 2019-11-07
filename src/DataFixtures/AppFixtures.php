08629eae2a1cb754e21259296e524287ad1a8f0b
t7sg <tonigalczeskij@gmail.com>
2019-11-07 21:58:14 +0300

    add user

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,34 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AppFixtures extends Fixture
 {
+    /** @var UserPasswordEncoderInterface $encoder */
+    private $encoder;
+
+    public function __construct(
+        UserPasswordEncoderInterface $encoder
+    ) {
+        $this->encoder          = $encoder;
+    }
+
     public function load(ObjectManager $manager)
     {
-        // $product = new Product();
-        // $manager->persist($product);
+
+        $user = new User();
+        $user
+            ->setUsername('username')
+            ->setPassword($this->encoder->encodePassword(
+                $user,
+                'password'
+            ));
+
+        $manager->persist($user);
 
         $manager->flush();
     }
